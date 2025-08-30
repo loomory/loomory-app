bb2e8f8e233bd34d2049cb36ab1e2096a1ee78eb
rasmuslantz <rasmus.lantz@gmail.com>
2025-08-30 13:00:09 +0200

    chore: remove dummy appbar

--- lib/pages/add_photos/add_photos.page.dart
+++ lib/pages/add_photos/add_photos.page.dart
@@ -2,16 +2,13 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/domain/models/timeline.model.dart';
-import 'package:immich_mobile/presentation/widgets/memory/memory_lane.widget.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/timeline.widget.dart';
-import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
-import 'package:immich_mobile/providers/user.provider.dart';
-import 'package:loomory/widgets/common/dummy_app_bar.dart';
 
 @RoutePage()
 class AddPhotosPage extends ConsumerWidget {
   const AddPhotosPage({super.key});
 
+  // check drift_search_page for example on overriding timeline provider, but we will still need a custom Timeline for main page
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return SafeArea(
@@ -26,7 +23,7 @@ class AddPhotosPage extends ConsumerWidget {
               child: Text("back"),
             ),
             Expanded(
-              child: Timeline(key: const Key("add-photos"), appBar: DummySliverAppBar(), groupBy: GroupAssetsBy.month),
+              child: Timeline(key: const Key("add-photos"), appBar: null, groupBy: GroupAssetsBy.none),
             ),
           ],
         ),
