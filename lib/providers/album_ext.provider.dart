13dde999af9d5da0142c5cb3397ba3d98fdb68a6
rasmuslantz <rasmus.lantz@gmail.com>
2025-10-09 17:16:47 +0200

    fix: correct count in album screen

--- lib/providers/album_ext.provider.dart
+++ lib/providers/album_ext.provider.dart
@@ -44,8 +44,8 @@ class AlbumExtNotifier extends StateNotifier<UploadListenerState> {
         if (remoteAlbum != null && remoteAlbum.thumbnailAssetId == null) {
           _log.info("First local asset uploaded to album without thumbnail");
           await _ref.read(remoteAlbumServiceProvider).updateAlbum(event.albumId, thumbnailAssetId: event.assetId);
-          await _ref.read(remoteAlbumProvider.notifier).refresh();
         }
+        await _ref.read(remoteAlbumProvider.notifier).refresh();
       case UploadProgressEvent():
         // Not used in the UI yet, would only be able to show how many uploads are left to process.
         _log.fine('Uploads pending: ${event.totalPending}');
@@ -53,12 +53,18 @@ class AlbumExtNotifier extends StateNotifier<UploadListenerState> {
     }
   }
 
+  // CreateAlbum will always refresh remoteAlbumProvider, even if there are no remote assets
   Future<void> createAlbum(String title, Set<BaseAsset> selectedAssets) async {
     return _ref.read(albumExtServiceProvider).createAlbum(title, selectedAssets);
   }
 
-  Future<void> addToAlbum(RemoteAlbum album, Set<BaseAsset> selectedAssets) {
-    return _ref.read(albumExtServiceProvider).addToAlbum(album, selectedAssets);
+  // addToAlbum will refresh remoteAlbumProvider through the uploadEvent if there are local assets.
+  // For remoteAssets, addition is instant but remoteAlbumProvider will not be refreshed so
+  // we do that manually. This means if there are both local and remote assets, we do an almost
+  // instant refresh here for the remote assets, then the local ones will refresh in _handleUploadEvent.
+  Future<void> addToAlbum(RemoteAlbum album, Set<BaseAsset> selectedAssets) async {
+    await _ref.read(albumExtServiceProvider).addToAlbum(album, selectedAssets);
+    return _ref.read(remoteAlbumProvider.notifier).refresh();
   }
 
   @override
